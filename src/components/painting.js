f61d0006920153abce49d60ccf0ac03be78381e2
Eric <Aerelioyanadel@gmail.com>
2020-03-17 19:23:19 -0400

    extract whole file folder of paintings images

--- src/components/painting.js
+++ src/components/painting.js
@@ -1,53 +1,44 @@
 import React from "react"
-// import { StaticQuery } from "gatsby"
-// import { StaticQuery } from "gatsby"
-import { graphql } from "gatsby"
+import { graphql, StaticQuery } from "gatsby"
 
-// import Img from "gatsby-image"
-import PropTypes from "prop-types"
 import Img from "gatsby-image"
 
-const Painting = ({ data }) => (
+export default () => (
   <section>
-    <p>this is some text from the painting page</p>
-    <Img fluid={data.allFile.edges.childImageSharp.fluid} alt="test2" />
-  </section>
-)
-
-Painting.propTypes = {
-  data: PropTypes.object.isRequired,
-}
-
-export const paintingQuery = graphql`
-  {
-    allFile(
-      filter: {
-        extension: { regex: "/(jpg)|(jpeg)|(png)/" }
-        relativeDirectory: { eq: "paintings" }
-      }
-    ) {
-      edges {
-        node {
-          id
-          name
-          birthTime
-          childImageSharp {
-            fluid {
-              # base64
-              # tracedSVG
-              # srcWebp
-              # srcSetWebp
-              # originalImg
-              # originalName
-              # presentationWidth
-              # presentationHeight
-              ...GatsbyImageSharpFluid
+    <StaticQuery
+      query={graphql`
+        {
+          allFile(
+            filter: {
+              extension: { regex: "/(jpg)|(jpeg)|(png)/" }
+              relativeDirectory: { eq: "paintings" }
+            }
+          ) {
+            edges {
+              node {
+                id
+                name
+                birthTime
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
             }
           }
         }
-      }
-    }
-  }
-`
-
-export default Painting
+      `}
+      render={data => (
+        <div>
+          {data.allFile.edges.map(edge => (
+            <Img
+              className="paintings"
+              fluid={edge.node.childImageSharp.fluid}
+            />
+          ))}
+        </div>
+      )}
+    />
+  </section>
+)
