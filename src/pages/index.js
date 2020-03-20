3c07fac6a06666ce1a70103ac6f30f6444edc55b
Eric <Aerelioyanadel@gmail.com>
2020-03-20 11:34:21 -0400

    remove gatsby starter code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
@@ -9,7 +9,7 @@ import Nav from "../components/nav"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Link to="/page-2/">Go to page 2</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
     <Nav />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       {/* <Image /> */}
